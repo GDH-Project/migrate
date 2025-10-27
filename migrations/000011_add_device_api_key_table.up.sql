4b12c63323660730dd8a6a601eca6b1b18649eda
SOPLAY <tkdgns1341@gmail.com>
2025-10-27 23:13:26 +0900

    feat(sql): device api_key 발급을 위한 테이블 내용 수정
    - ref #5

--- migrations/000011_add_device_api_key_table.up.sql
+++ migrations/000011_add_device_api_key_table.up.sql
@@ -1,6 +1,13 @@
 CREATE TABLE device.api_key (
     id             VARCHAR(32) PRIMARY KEY,
-    device_info_id uuid NOT NULL,
+    device_info_id uuid                      NOT NULL,
+    title          VARCHAR(255)              NOT NULL,
+    description    TEXT,
+    created_at     TIMESTAMPTZ DEFAULT NOW() NOT NULL,
 
     CONSTRAINT fk_device_info_id FOREIGN KEY (device_info_id) REFERENCES device.device_info (id) ON DELETE NO ACTION
-)
+);
+
+-- 인덱스
+
+CREATE INDEX IF NOT EXISTS idx_device_api_key_device_info_id ON device.api_key (device_info_id);
