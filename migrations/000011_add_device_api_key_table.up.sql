a6e41f9cf9920166b779ace491213474ad1ab091
SOPLAY <tkdgns1341@gmail.com>
2025-10-28 10:33:58 +0900

    feat(sql): device api_key 발급을 위한 테이블 내용 고유 id 및 userID 추가
    - ref #5

--- migrations/000011_add_device_api_key_table.up.sql
+++ migrations/000011_add_device_api_key_table.up.sql
@@ -1,11 +1,14 @@
 CREATE TABLE device.api_key (
-    id             VARCHAR(32) PRIMARY KEY,
+    id             SERIAL PRIMARY KEY,
+    user_id        UUID                      NOT NULL,
+    api_key        VARCHAR(32) UNIQUE        NOT NULL,
     device_info_id uuid                      NOT NULL,
     title          VARCHAR(255)              NOT NULL,
     description    TEXT,
     created_at     TIMESTAMPTZ DEFAULT NOW() NOT NULL,
 
-    CONSTRAINT fk_device_info_id FOREIGN KEY (device_info_id) REFERENCES device.device_info (id) ON DELETE NO ACTION
+    CONSTRAINT fk_device_info_id FOREIGN KEY (device_info_id) REFERENCES device.device_info (id) ON DELETE NO ACTION,
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES auth.users (id) ON DELETE NO ACTION
 );
 
 -- 인덱스
